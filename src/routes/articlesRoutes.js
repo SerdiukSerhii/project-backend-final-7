2e8cc66dd1588e842093d619fe2f094508009810
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-08-07 22:38:48 +0300

    Add authentication middleware

--- src/routes/articlesRoutes.js
+++ src/routes/articlesRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { getSavedArticles } from '../controllers/articles/getSavedArticles.js';
-import { authenticate } from '../middleware/authenticate.js'; 
+import { authenticate } from '../middleware/authenticate.js';
 
 const articlesRouter = Router();
 
-articlesRouter.get('/saved', authenticate, getSavedArticles);
+articlesRouter.use(authenticate);
+
+articlesRouter.get('/saved', getSavedArticles);
 
 export default articlesRouter;
