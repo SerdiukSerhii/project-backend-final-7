6fd2469ac15b47b34549c1df1d9ccdd4b37eef36
Alina Rozental <dev.alinaro@gmail.com>
2026-08-07 13:06:14 +0200

    feat: add public endpoint for user created articles

--- src/routes/articlesRoutes.js
+++ src/routes/articlesRoutes.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import { getUserArticles } from '../controllers/articles/articlesControllers.js';
 
 const articlesRouter = Router();
+
+articlesRouter.get('/articles/user/:userId', getUserArticles);
 
 export default articlesRouter;
