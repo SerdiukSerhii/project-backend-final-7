e229d4b27d37afa67a21bdb1cd189babbaffa2c6
Alina <alinavinnikhgfff@gmail.com>
2026-08-07 17:18:33 +0300

    add contoller and endpoint updateUserAvatar

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,12 @@
 import { registerUser } from './auth/registerUser.js';
 import { loginUser } from './auth/loginUser.js';
+import { updateUserAvatar } from './users/updateUserAvatar.js';
 
 export const auth = {
   registerUser,
   loginUser,
+};
+
+export const users = {
+  updateUserAvatar,
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,13 @@
 import { Router } from 'express';
+import { users as ctrl } from '../controllers/index.js';
+import { upload } from '../middleware/multer.js';
 
 const userRouter = Router();
+
+userRouter.patch(
+  '/users/me/avatar',
+  upload.single('avatar'),
+  ctrl.updateUserAvatar,
+);
 
 export default userRouter;
