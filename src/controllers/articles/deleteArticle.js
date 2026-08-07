3b1cf6aa5b8de13599e00437a8ed06b3c234b7a0
Alina <alinavinnikhgfff@gmail.com>
2026-08-07 20:53:09 +0300

    add deleteArticle controller

--- src/controllers/articles/deleteArticle.js
+++ src/controllers/articles/deleteArticle.js
@@ -1,1 +1,15 @@
-export const deleteArticle = () => {};
+import createHttpError from 'http-errors';
+import { Article } from '../../models/article.js';
+
+export const deleteArticle = async (req, res) => {
+  const { articleId } = req.params;
+  const article = await Article.findOneAndDelete({
+    _id: articleId,
+    userId: req.user._id,
+  });
+  if (!article) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(article);
+};
