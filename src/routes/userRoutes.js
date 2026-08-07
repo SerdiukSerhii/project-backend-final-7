bf86d87a940b6f199b22f1f524d1c39154e39737
OlyaBorzynska <mykytlo@gmail.com>
2026-08-07 16:25:32 +0300

    add endpoint get-user-by-id

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import { user as ctrl } from '../controllers/index.js';
+import { userIdSchema } from '../validations/index.js';
+import { celebrate } from 'celebrate';
 
 const userRouter = Router();
+
+userRouter.get('/users/:userId', celebrate(userIdSchema), ctrl.getUserById);
 
 export default userRouter;
--- src/validations/index.js
+++ src/validations/index.js
@@ -1,4 +1,5 @@
 import { registerUserSchema } from './auth/registerUser.js';
 import { loginUserSchema } from './auth/loginUser.js';
+import { userIdSchema } from './users/userId.js';
 
-export { registerUserSchema, loginUserSchema };
+export { registerUserSchema, loginUserSchema, userIdSchema };
