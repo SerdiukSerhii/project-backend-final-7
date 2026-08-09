551a0fc976f2e74d48ae5a436a661080782e25fb
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-08-09 23:13:54 +0300

    configure frontend API integration

--- src/routes/articlesRoutes.js
+++ src/routes/articlesRoutes.js
@@ -7,8 +7,11 @@ import { authenticate } from '../middleware/authenticate.js';
 import { articleIdSchema } from '../validations/articles/deleteArticle.js';
 import { createArticle } from '../controllers/articles/createArticle.js';
 import { upload } from '../middleware/multer.js';
-import { getArticleByIdSchema, getAllArticlesSchema, editArticleSchema } from '../validations/index.js';
-export { updateArticleSchema } from './articles/updateArticle.js';
+import {
+  getArticleByIdSchema,
+  getAllArticlesSchema,
+  editArticleSchema,
+} from '../validations/index.js';
 
 const articlesRouter = Router();
 
@@ -16,11 +19,15 @@ articlesRouter.get('/articles/saved', authenticate, getSavedArticles);
 articlesRouter.delete(
   '/articles/delete',
   authenticate,
-    celebrate(articleIdSchema),
+  celebrate(articleIdSchema),
   ctrl.deleteArticle,
 );
 
-articlesRouter.get('/articles',celebrate(getAllArticlesSchema), ctrl.getAllArticles);
+articlesRouter.get(
+  '/articles',
+  celebrate(getAllArticlesSchema),
+  ctrl.getAllArticles,
+);
 
 articlesRouter.post('/', authenticate, upload.single('img'), createArticle);
 articlesRouter.get('/articles/user/:userId', getUserArticles);
@@ -34,8 +41,8 @@ articlesRouter.get(
 articlesRouter.patch(
   '/articles/:articleId',
   authenticate,
-  celebrate(editArticleSchema),  
-  ctrl.editArticle               
+  celebrate(editArticleSchema),
+  ctrl.editArticle,
 );
 
 export default articlesRouter;
