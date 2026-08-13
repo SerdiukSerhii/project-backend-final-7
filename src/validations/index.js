be413bd903cdd40336a36e1f06f657f0bb399300
Karina <karina24032006.09@gmail.com>
2026-08-14 01:43:41 +0300

    feat: add GET /authors endpoint with pagination

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,5 +1,6 @@
 import { registerUserSchema } from './auth/registerUser.js';
 import { loginUserSchema } from './auth/loginUser.js';
 import { userIdSchema } from './users/userId.js';
+import { getAllAuthorsSchema } from './authors/getAllAuthors.js';
 
-export { registerUserSchema, loginUserSchema, userIdSchema };
+export { registerUserSchema, loginUserSchema, userIdSchema, getAllAuthorsSchema };
