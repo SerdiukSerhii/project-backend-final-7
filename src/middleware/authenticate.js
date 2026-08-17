32e4e644f6d0651f656649fb27c1b60a24a7d234
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-08-17 13:54:38 +0300

    debug logout authentication

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,9 +1,50 @@
+// import createHttpError from 'http-errors';
+// import { User } from '../models/user.js';
+// import { Session } from '../models/session.js';
+
+// export const authenticate = async (req, res, next) => {
+//   const { sessionId, accessToken } = req.cookies;
+
+//   if (!sessionId || !accessToken) {
+//     throw createHttpError(401, 'Missing access token');
+//   }
+
+//   const session = await Session.findOne({
+//     _id: sessionId,
+//     accessToken,
+//   });
+
+//   if (!session) {
+//     throw createHttpError(401, 'Session not found');
+//   }
+
+//   const isAccessTokenExpired = session.accessTokenValidUntil < new Date();
+//   if (isAccessTokenExpired) {
+//     throw createHttpError(401, 'Access token expired');
+//   }
+
+//   const user = await User.findOne({ _id: session.userId });
+//   if (!user) {
+//     throw createHttpError(401, 'User not found');
+//   }
+
+//   req.user = user;
+//   next();
+// };
+
 import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import { Session } from '../models/session.js';
 
 export const authenticate = async (req, res, next) => {
   const { sessionId, accessToken } = req.cookies;
+
+  console.log('AUTH:', {
+    hasSessionId: Boolean(sessionId),
+    hasAccessToken: Boolean(accessToken),
+    sessionId,
+    accessTokenPrefix: accessToken?.slice(0, 8),
+  });
 
   if (!sessionId || !accessToken) {
     throw createHttpError(401, 'Missing access token');
@@ -14,16 +55,20 @@ export const authenticate = async (req, res, next) => {
     accessToken,
   });
 
+  console.log('SESSION FOUND:', Boolean(session));
+
   if (!session) {
     throw createHttpError(401, 'Session not found');
   }
 
   const isAccessTokenExpired = session.accessTokenValidUntil < new Date();
+
   if (isAccessTokenExpired) {
     throw createHttpError(401, 'Access token expired');
   }
 
   const user = await User.findOne({ _id: session.userId });
+
   if (!user) {
     throw createHttpError(401, 'User not found');
   }
